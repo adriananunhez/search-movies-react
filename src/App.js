70c58616419b5175c01804ebf8e9c14b64f140d7
adriananunhez <adri.nunhez@gmail.com>
2020-06-14 09:48:29 -0400

    First commit basic

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import { Detail } from './pages/Detail'
+import { Home } from './pages/Home'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/detail/:id' component={Detail}/>
+        </Switch>
+      </div>
+      );
+  }
+}
+
+export default App;
